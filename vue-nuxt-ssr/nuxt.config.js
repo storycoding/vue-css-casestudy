b084920bc2ce8ad7474d302b096a46be85fba98e
storycoding <youknownuno@gmail.com>
2018-08-23 15:50:19 +0100

    fix: replaced vue-nuxt-ssr community package with vuetify/nuxt package

--- vue-nuxt-ssr/nuxt.config.js
+++ vue-nuxt-ssr/nuxt.config.js
@@ -1,3 +1,6 @@
+const nodeExternals = require('webpack-node-externals')
+const resolve = (dir) => require('path').join(__dirname, dir)
+
 module.exports = {
   /*
   ** Headers of the page
@@ -7,12 +10,17 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'project designed to test css injection' }
+      { hid: 'description', name: 'description', content: 'Nuxt.js + Vuetify.js project' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons' }
     ]
   },
+  plugins: ['~/plugins/vuetify.js'],
+  css: [
+    '~/assets/style/app.styl'
+  ],
   /*
   ** Customize the progress bar color
   */
@@ -21,11 +29,25 @@ module.exports = {
   ** Build configuration
   */
   build: {
+    babel: {
+      plugins: [
+        ["transform-imports", {
+          "vuetify": {
+            "transform": "vuetify/es5/components/${member}",
+            "preventFullImport": true
+          }
+        }]
+      ]
+    },
+    vendor: [
+      '~/plugins/vuetify.js'
+    ],
+    extractCSS: true,
     /*
     ** Run ESLint on save
     */
-    extend (config, { isDev, isClient }) {
-      if (isDev && isClient) {
+    extend (config, ctx) {
+      if (ctx.isDev && ctx.isClient) {
         config.module.rules.push({
           enforce: 'pre',
           test: /\.(js|vue)$/,
@@ -33,7 +55,13 @@ module.exports = {
           exclude: /(node_modules)/
         })
       }
+      if (ctx.isServer) {
+        config.externals = [
+          nodeExternals({
+            whitelist: [/^vuetify/]
+          })
+        ]
+      }
     }
   }
 }
-
